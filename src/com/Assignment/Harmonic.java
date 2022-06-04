ad0cbbe26c4896f23a2414f6c3bc73b480cab63c
mdMustafa <cmustafa933@gmail.com>
2022-06-04 08:16:49 +0530

    assignment

--- src/com/Assignment/Harmonic.java
+++ src/com/Assignment/Harmonic.java
@@ -1,18 +1,23 @@
 package com.Assignment;
 
-public class Harmonic { 
-		   public static void main(String[] args) { 
-		 
-		      int x = Integer.parseInt(args[0]);
-		 
-		      // compute 1/1 + 1/2 + 1/3 + ... + 1/x
-		      double sum = 0.0;
-		      for (int i = 1; i <= x; i++) {
-		            //sum += 1.0 / i;
-		         sum = sum + (1.0/i);
-		      }
-		 
-		      System.out.println(sum);
-		}
+import java.util.Scanner;
 
-}
+public class Harmonic { 
+	public static void main(String... a)
+	    {
+	        System.out.print("Enter any number : ");
+	        Scanner s = new Scanner(System.in);
+	        int num = s.nextInt();
+	        System.out.print("The Harmonic Series is : ");
+	        double result = 0.0;
+	        while(num > 0)
+	          {
+	               result = result + (double) 1 / num;
+	               num--;
+	               System.out.print(result +"  ");
+	          }
+	        System.out.println("");
+	 
+	        System.out.println("Output of Harmonic Series is "+result);
+	    }
+	}
